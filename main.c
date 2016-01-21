e1c5ac1a2451dca47cf03256bfd055161ba926e3
Joel Ekström <accatyyc@gmail.com>
2016-01-21 23:50:39 +0100

    Draw full screen quad so all rendering can happen in frag shaders

--- main.c
+++ main.c
@@ -3,26 +3,30 @@
 #import <stdlib.h>
 
 GLfloat vertices[] = {
-	0.0, 0.0,
-	0.5, 0.0,
-	0.5, 0.5
+	-1.0, -1.0,
+	1.0, -1.0,
+	-1.0, 1.0,
+	1.0, 1.0
 };
 
 const char *vertex_shader =
 "#version 150\n"
-"in vec4 position;"
+"in vec4 in_vertex;"
 
 "void main() {"
-"    gl_Position = position;"
+"    gl_Position = in_vertex;"
 "}";
 
 
 const char *fragment_shader =
 "#version 150\n"
+	//"in vec2 mouse_position;"
+"uniform int width;"
+"uniform int height;"
 "out vec4 out_color;"
 
 "void main() {"
-"    out_color = vec4(1.0, 1.0, 1.0, 1.0);"
+"    out_color = vec4(1.0, gl_FragCoord.x / width, 1.0, 1.0);"
 "}";
 
 GLuint compile_shader(const char *src, GLenum shader_type);
@@ -33,52 +37,52 @@ GLFWwindow *setup_window(int width, int height, const char *title);
 
 int main()
 {
-    /* Initialize the library */
     if (!glfwInit()) {
         return -1;
 	}
 
 	GLFWwindow *window = setup_window(640, 480, "Hellurr!!");
 	print_GL_version(window);
-
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
-	glClearColor(1.0, 0.0, 0.0, 1.0);
 
-	/* Create Vertex buffer object */
+	// Create Vertex buffer object, containing our 4 vertices.
+	// This allows us to draw a full screen quad, so we can
+	// later on do raycasting in the fragment shader
 	GLuint vbo;
 	glGenBuffers(1, &vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, vbo);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-	/* Compile shaders */
+	// Compile shaders. The vertex shader is just pass-through
 	GLuint v_shader = compile_shader(vertex_shader, GL_VERTEX_SHADER);
 	GLuint f_shader = compile_shader(fragment_shader, GL_FRAGMENT_SHADER);
-
-	/* Create a shader program */
 	GLuint program = create_program(v_shader, f_shader);
 
-	/* Bind shader attributes with a vertex array object */
+	// Bind shader attributes with a vertex array object
 	GLuint vao;
 	glGenVertexArrays(1, &vao);
   	glBindVertexArray(vao);
-	GLint position_attribute = glGetAttribLocation(program, "position");
-	glVertexAttribPointer(position_attribute, 2, GL_FLOAT, GL_FALSE, 0, 0);
-	glEnableVertexAttribArray(position_attribute);
+	
+	GLint vertex_handle = glGetAttribLocation(program, "in_vertex");
+	glVertexAttribPointer(vertex_handle, 2, GL_FLOAT, GL_FALSE, 0, 0);
+	glEnableVertexAttribArray(vertex_handle);
 
-    /* Loop until the user closes the window */
+   	// Pass the frame buffer size to the fragment shader
+	int width, height;
+	glfwGetFramebufferSize(window, &width, &height);
+	GLint width_handle = glGetUniformLocation(program, "width");
+	glUniform1i(width_handle, width);
+	
+    // Loop until the user closes the window
     while (!glfwWindowShouldClose(window))
     {
-        /* Render here */
-		glClear(GL_COLOR_BUFFER_BIT);
+		// Draw our full screen quad
 		glBindVertexArray(vao);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
 
-        /* Swap front and back buffers */
+        // Swap front and back buffers
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
+		glfwPollEvents();
     }
 
     glfwTerminate();
@@ -88,6 +92,7 @@ int main()
 
 GLFWwindow *setup_window(int width, int height, const char *title)
 {
+	/* Make sure we run at least OpenGL 3.2 */
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
  	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
  	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
