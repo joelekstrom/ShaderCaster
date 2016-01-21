bb3afab16f032f9d4cd032114504bcbfee8b5552
Joel Ekström <accatyyc@gmail.com>
2016-01-21 12:22:03 +0100

    Add basic render loop

--- main.c
+++ main.c
@@ -1,6 +1,36 @@
 #import <stdio.h>
+#import <GLFW/glfw3.h>
 
 int main(int argc, char **argv)
 {
+    /* Initialize the library */
+    if (!glfwInit()) {
+        return -1;
+	}
+
+    /* Create a windowed mode window and its OpenGL context */
+    GLFWwindow *window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window) {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
 	return 0;
 }
